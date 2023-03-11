886fd52a890f4bdb326888f0847df2e103aa266c
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-03-11 17:01:29 +0530

    Add files via upload

---
+++ assignment_14.py
@@ -0,0 +1,29 @@
+def is_up(x):
+    for i in x:
+        if 'A'<=i and i<='Z':
+            return True
+    return False
+def is_low(x):
+    for i in x:
+        if 'a'<=i and i<='z':
+            return True
+    return False
+def is_num(x):
+    for i in x:
+        if '0'<=i and i<='9':
+            return True
+    return False
+def is_other(x):
+    for i in x:
+        if i=='@' or i=='#' or i == '$':
+            return True
+    return False
+
+a = input('enter password:\n').split(',')
+lst = []
+for i in a :
+    length = len(i)
+    if 6<=length and length<=12 and is_low(i) and is_num(i) and is_up(i) and is_other(i):
+        lst.append(i)
+
+print(','.join(lst))
